ac7ed1d1dbbddc094ace5a5a869ba981a8fa2ff8
Julie Rymer <rymerjulie.pro@gmail.com>
2024-02-20 13:01:58 +0100

    feat: basic routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, Injectable } from '@angular/core';
+import {
+  provideRouter,
+  RouterStateSnapshot,
+  TitleStrategy,
+  withComponentInputBinding,
+} from '@angular/router';
 
 import { routes } from './app.routes';
+import { Title } from '@angular/platform-browser';
+
+@Injectable({ providedIn: 'root' })
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`Poker Planner Lite | ${title}`);
+    }
+  }
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    { provide: TitleStrategy, useClass: TemplatePageTitleStrategy },
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LobbyComponent } from './lobby/lobby.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'lobby', title: 'Lobby', component: LobbyComponent },
+  { path: '', redirectTo: '/lobby', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
